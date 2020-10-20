10a22a8f3b0b9b3363f3546c91bb274801474dee
treknuts <trevor.a.knutson@ndsu.edu>
2020-10-20 14:04:17 -0500

    Implemented service to consume the server api for projects list. WIP display projects as card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,9 +7,11 @@ import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './projects/projects.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProjectsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -16,6 +19,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,23 @@
+import { JsonPipe } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProjectsService } from '../service/projects.service';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
+})
+export class ProjectsComponent implements OnInit {
+  @Input()
+  results: Observable<any>;
+
+  constructor(private projectsService: ProjectsService) {
+    let projects: string[] = [];
+    projectsService.resolveProjects().subscribe((data) => {
+      this.results = data.data;
+    });
+  }
+
+  ngOnInit(): void {}
+}
