b59f9a5a95a80840fb103046a7486ac4ea9d775a
treknuts <trevor.a.knutson@ndsu.edu>
2020-11-11 11:24:33 -0600

    Trying to fix navbar on ipad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
   {
     path: 'about',
     component: AboutComponent,
